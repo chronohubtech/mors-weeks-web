f7db128c34cc127c7f63520f4500dba0396c3854
Kurono <chrono.social@outlook.com>
2022-12-21 22:10:47 +0800

    add: idea implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import moment from "moment";
+import { range } from "range";
+
+const birthDate = new Date("July 26, 2002");
+const dateToday = new Date();
+
+const birthDateMoment = moment(birthDate);
+const dateTodayMoment = moment(dateToday);
+
+const intervalTime = moment.duration(dateTodayMoment.diff(birthDateMoment));
+
+const weeksAlive = intervalTime.asWeeks().toFixed(0);
+const minWeeksOfLife = 4500;
+const weeksLeftInYourLife = minWeeksOfLife - weeksAlive;
+
+const weeksAliveArray = range(1, weeksAlive);
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">{weeksAlive} weeks alive</h1>
+  );
 }
